318e3eadcf951f07f741453c46ffacab1fb7e1c7
ponte1010 <t.miwa.528@nitech.jp>
2021-01-12 21:43:26 +0900

    [add]ダブルタップしたら座標を音声出力

--- iPadxTG-DoubleTap/ViewController.swift
+++ iPadxTG-DoubleTap/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var tapLocation: CGPoint = CGPoint()
+    
+    // AVSpeechSynthesizerのインスタンスを生成
+    let synthesizer = AVSpeechSynthesizer()
+    
+    var strTapPoints: String = ""
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // タッチイベントを取得する
+        let touch = touches.first
+        // タップした座標を取得
+        tapLocation = touch!.location(in: self.view)
+        
+        let xTap = tapLocation.x
+        let yTap = tapLocation.y
+        
+        let intxTap:Int = Int(xTap)
+        let intyTap:Int = Int(yTap)
+        
+        let strxTap:String = String(intxTap)
+        let stryTap:String = String(intyTap)
+        
+        strTapPoints = "エックス" + strxTap + "ワイ" + stryTap
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
+        recognizer.numberOfTouchesRequired = 1
+        recognizer.numberOfTapsRequired = 2
+        view.addGestureRecognizer(recognizer)
     }
-
-
+    
+    @IBAction func handleGesture(_ gesture: UITapGestureRecognizer) {
+        guard gesture.state == .ended else {
+            return
+        }
+        // 確認用
+        print(strTapPoints)
+        
+        let utterance = AVSpeechUtterance.init(string: strTapPoints)
+        let voice = AVSpeechSynthesisVoice.init(language: "ja-JP")
+        utterance.voice = voice
+        synthesizer.speak(utterance)
+    }
+    
+    
 }
-
